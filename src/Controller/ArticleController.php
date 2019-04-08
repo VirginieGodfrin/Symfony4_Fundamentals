16e00c94ebcf6c61ecd31ae13443d9fc3d5045c6
VirginieGodfrin <virginiegodfrin@gmail.com>
2019-04-08 13:52:07 +0200

    initial comit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('article/homepage.html.twig');
+    }
+
+    /**
+     * @Route("/news/{slug}", name="article_show")
+     */
+    public function show($slug)
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
+            'comments' => $comments,
+        ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        // TODO - actually heart/unheart the article!
+
+        $logger->info('Article is being hearted!');
+
+        return new JsonResponse(['hearts' => rand(5, 100)]);
+    }
+}
