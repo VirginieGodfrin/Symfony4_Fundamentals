d0f07440db469515f71a0d478492080bf735437f
VirginieGodfrin <virginiegodfrin@gmail.com>
2019-04-08 14:32:47 +0200

    Chapter 2: KnpMarkdownBundle & its Services

--- config/bundles.php
+++ config/bundles.php
@@ -1,11 +1,5 @@
 <?php
 
-// Symfony is really nothing more than a collection of services.
-// Every service is stored inside another object called the container.
-// Bundles are what actually prepare those service objects and put them into the container.
-// Bundles are Symfony's plugin system.
-// To get a list of the services that we can access :  ./bin/console debug:autowiring
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
@@ -14,4 +8,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
 ];
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
+use Michelf\MarkdownInterface;
 
 class ArticleController extends AbstractController
 {
@@ -22,17 +23,32 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug)
+    public function show($slug,  MarkdownInterface $markdown)
     {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
+        // article content set with the multiline HEREDOC syntax
+        $articleContent = <<<EOF
+Bacon ipsum dolor amet jowl chuck sausage corned beef. **Shoulder burgdoggen sausage**, ball tip turkey venison hamburger ground round.[beef ribs](https://baconipsum.com/). Short ribs buffalo jerky <em>kielbasa</em> ham hock biltong pork chop bresaola. <p>Chuck porchetta jowl swine, tri-tip frankfurter pig corned beef bacon rump bresaola. Prosciutto pig jowl beef swine meatloaf cow biltong sausage ham hock boudin short loin doner. Drumstick shank ribeye venison picanha.</p>
+
+Spare ribs burgdoggen porchetta jerky ribeye tenderloin prosciutto cow corned beef meatloaf beef sirloin jowl salami. Brisket t-bone boudin picanha pork belly cow bacon shankle ground round prosciutto tenderloin jerky short loin hamburger. Pancetta chicken rump landjaeger shankle kielbasa. Bacon brisket ribeye, pork belly biltong chuck turkey kevin ham pancetta buffalo. Fatback ribeye venison landjaeger turducken.
+
+Pancetta sausage t-bone andouille salami pastrami turkey. Spare ribs ham hock doner, beef turkey venison landjaeger. Corned beef picanha t-bone salami strip steak. Pork chop ground round drumstick shoulder biltong andouille rump strip steak alcatra chuck meatloaf tongue flank. Strip steak drumstick beef ribs landjaeger tenderloin shank prosciutto filet mignon pork chop turducken meatloaf porchetta pork loin.
+
+Turducken landjaeger pork loin short ribs hamburger meatball chuck pastrami shoulder chicken. Buffalo rump salami pork chop ham hock fatback landjaeger. Doner leberkas bacon venison beef ribs swine sirloin pastrami jowl biltong kielbasa alcatra picanha jerky. Chuck ham hock ribeye spare ribs short ribs pig, swine tenderloin hamburger bresaola turducken shank pork chop chicken.
+
+Tail short ribs rump swine short loin brisket prosciutto, ham beef tenderloin. Burgdoggen ham spare ribs, leberkas chicken tri-tip tongue chuck rump capicola ribeye ham hock pork belly kielbasa. Chicken shankle cow burgdoggen salami ham brisket pork chop tenderloin prosciutto frankfurter pancetta cupim jerky picanha. Sausage hamburger ground round prosciutto tri-tip pork loin pork belly.
+EOF;
+        // Call the markdomn's transform method.
+        $articleContent = $markdown->transform($articleContent);
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
             'slug' => $slug,
+            'articleContent' => $articleContent,
             'comments' => $comments,
         ]);
     }
