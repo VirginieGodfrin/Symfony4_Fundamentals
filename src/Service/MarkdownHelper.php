00924226f840fd54a45984f13ab8dd536b8d0123
VirginieGodfrin <virginiegodfrin@gmail.com>
2019-04-08 20:22:19 +0200

    Chapter 8: Creating Services ok

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 use Michelf\MarkdownInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Psr\Log\LoggerInterface;
 
 // Never forget when you create a service class, the arguments to its constructor are autowired.
 // That means that we can use any of the classes or interfaces from debug:autowiring as type-hints.
@@ -13,21 +14,29 @@ class MarkdownHelper
 
 	private $markdown;
 
-	public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+	private $logger;
+
+	public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
 	{
 		$this->cache = $cache;
 		$this->markdown = $markdown;
+		$this->logger = $logger;
 	}
 
-	public function parse(string $source): string{
+	public function parse(string $source): string
+	{
 
-	    $item = $this->cache->getItem('markdown_'.md5($articleContent));
+		if (stripos($source, 'bacon') !== false) { 
+			$this->logger->info('They are talking about bacon again!');
+		}
+
+	    $item = $this->cache->getItem('markdown_'.md5($source));
 
 		if (!$item->isHit()) {
 
-			$item->set($this->markdown->transform($articleContent));
+			$item->set($this->markdown->transform($source));
 			$this->cache->save($item);
 		}
-		$articleContent = $item->get();		
+		return $item->get();		
 	}
 }
