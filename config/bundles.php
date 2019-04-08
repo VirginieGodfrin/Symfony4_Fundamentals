4a06ad2b613bf25b0bff357caf1783ef0e9f48fd
VirginieGodfrin <virginiegodfrin@gmail.com>
2019-04-08 14:10:48 +0200

    Chapter 1: Bundles give you Services

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,11 @@
 <?php
 
+// Symfony is really nothing more than a collection of services.
+// Every service is stored inside another object called the container.
+// Bundles are what actually prepare those service objects and put them into the container.
+// Bundles are Symfony's plugin system.
+// To get a list of the services that we can access :  ./bin/console debug:autowiring
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
