503f656f0d7b8a76217dbf92079011b109109ab0
VirginieGodfrin <virginiegodfrin@gmail.com>
2019-04-09 15:09:41 +0200

    Chapter 11: Config Parameters

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -16,28 +16,44 @@ class MarkdownHelper
 
 	private $logger;
 
+	private $isDebug;
+
 	// use bind (If you find any argument named $markdownLogger , pass this service to it.)
-	public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
+	// isDebug or an API key - those should also be passed as constructor arguments.
+	public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug)
 	{
 		$this->cache = $cache;
 		$this->markdown = $markdown;
 		$this->logger = $markdownLogger;
+		$this->isDebug = $isDebug;
 	}
 
 	public function parse(string $source): string
 	{
+		// dump($markdown);die;
+        // dump($this->cache);die;
 
+		// use the logger service
 		if (stripos($source, 'bacon') !== false) { 
 			$this->logger->info('They are talking about bacon again!');
 		}
 
-	    $item = $this->cache->getItem('markdown_'.md5($source));
+	    // skip caching entirely in debug
+	    // kenel debug is false in prod
+		if ($this->isDebug) {
+			return $this->markdown->transform($source);
+		}
+		// create cache item
+		$item = $this->cache->getItem('markdown_'.md5($source));
 
+		// set item with sources transformed with markdown
+		// save item in the cache
 		if (!$item->isHit()) {
-
 			$item->set($this->markdown->transform($source));
 			$this->cache->save($item);
 		}
+
+		// return item who contain sources
 		return $item->get();		
 	}
 }
